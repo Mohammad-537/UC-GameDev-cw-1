be2c589deefeeee2a87451d0a63492bcb4e07c9a
Mohammad-537 <66563296+Mohammad-537@users.noreply.github.com>
2022-06-22 20:14:27 +0300

    first-day
    pain

---
+++ GameDev-FirstDay/GameDev-FirstDay/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GameDev_FirstDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charecterHealth = 20;
+            string charecterName = "Leon";
+            double charecterSpeed = 2.5;
+            bool isCharcterAlive = true;
+            Console.WriteLine("Hi I am {0} and this is my health bar {1} and this is my speed {2} ms-1 let check if I am achallty alive the deive says {3}", charecterName, charecterHealth, charecterSpeed, isCharcterAlive);
+            
+        }
+    }
+}
